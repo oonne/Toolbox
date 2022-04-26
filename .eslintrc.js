e9fa8c3681a3cd00033a4b3431f742c904ccbb6a
hejiayi <hejiayi@kp99.cn>
2022-04-26 14:22:02 +0800

    chore: eslint+prettier 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'vue/setup-compiler-macros': true,
   },
   extends: [
-    'plugin:vue/essential',
+    'prettier',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb-base',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
