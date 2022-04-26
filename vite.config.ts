8de638c467fd20e6fe2aec3f7409fc3f46b6f73b
hejiayi <hejiayi@kp99.cn>
2022-04-26 18:22:38 +0800

    feat: 配置全局CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
