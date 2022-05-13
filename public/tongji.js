2432c8f550a8456857f4a78eaa2b0baca1b9c77e
hejiayi <hejiayi@kp99.cn>
2022-05-13 11:53:42 +0800

    feat: 新增统计功能

---
+++ public/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  const hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?ec90e7f1609d7b64d098140d82600bfb';
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+}());
