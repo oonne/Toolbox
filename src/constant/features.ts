46c12ad86bf8e23b2699dbdcdcc74cc2198150ac
hejiayi <hejiayi@kp99.cn>
2023-06-21 15:07:05 +0800

    feat: 适配移动端的首页

--- src/constant/features.ts
+++ src/constant/features.ts
@@ -2,49 +2,40 @@
 interface Features {
   name: string;
   label: string;
-  category: string[];
 }
 
 const featuresList: Features[] = [
   {
-    name: 'Base64',
-    label: 'Base64',
-    category: ['All', 'Encode'],
-  },
-  {
-    name: 'QRcode',
-    label: '二维码',
-    category: ['All', 'Other'],
-  },
-  {
-    name: 'MD5',
-    label: 'MD5',
-    category: ['All', 'Hash'],
-  },
-  {
-    name: 'SHA',
-    label: 'SHA',
-    category: ['All', 'Hash'],
-  },
-  {
     name: 'PBKDF2',
     label: 'PBKDF2',
-    category: ['All', 'Encrypt'],
+  },
+  {
+    name: 'Base64',
+    label: 'Base64',
   },
   {
     name: 'AES',
     label: 'AES',
-    category: ['All', 'Encrypt'],
   },
   {
     name: 'RSA',
     label: 'RSA',
-    category: ['All', 'Encrypt'],
+  },
+  {
+    name: 'MD5',
+    label: 'MD5',
+  },
+  {
+    name: 'SHA',
+    label: 'SHA',
+  },
+  {
+    name: 'QRcode',
+    label: '二维码',
   },
   {
     name: 'Password',
     label: '口令生成器',
-    category: ['All', 'Other'],
   },
 ];
 
--- src/pages/Index/IndexPage.vue
+++ src/pages/Index/IndexPage.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import categorys from '../../constant/category';
 import features from '../../constant/features';
-
-/* 分类 */
-const active = ref('All');
-const SetCategory = (en: string) => {
-  active.value = en;
-};
-
-/* 功能 */
-const featuresList = computed(() => features.filter((f) => f.category.includes(active.value)));
 </script>
 
 <template>
@@ -20,28 +9,10 @@ const featuresList = computed(() => features.filter((f) => f.category.includes(a
       加一工具箱
     </header>
 
-    <!-- 筛选 -->
-    <div class="category-warp">
-      <ul class="category">
-        <li
-          v-for="category in categorys"
-          :key="category.en"
-          class="category-item"
-          :class="category.en===active ? 'active' : ''"
-          @click="SetCategory(category.en)"
-        >
-          <p>{{ category.cn }}</p>
-          <p class="category-en">
-            {{ category.en }}
-          </p>
-        </li>
-      </ul>
-    </div>
-
     <!-- 功能 -->
     <div class="feature-warp">
       <router-link
-        v-for="item in featuresList"
+        v-for="item in features"
         :key="item.name"
         :to="{ name: item.name }"
         class="feature-item"
@@ -56,56 +27,28 @@ const featuresList = computed(() => features.filter((f) => f.category.includes(a
 .index{
   margin: auto;
   padding-top: 3rem;
-  max-width: 768px;
+  max-width: 1200px;
 }
 
 .logo{
   font-family: 'MFQiHei';
-  font-size: 5rem;
+  font-size: 3rem;
   text-align: center;
   margin-bottom: 1.5rem;
 }
-
-.category-warp{
-  overflow: auto;
-}
-.category-warp::-webkit-scrollbar {
-  display: none;
-}
-.category{
-  display: flex;
-  background-color: var(--content-stacked-background);
-  width: 768px;
-}
-.category-item{
-  list-style: none;
-  padding: 16px;
-  width: 128px;
-  cursor: pointer;
-  text-align: center;
-}
-.category-en{
-  font-size: 0.8rem;
-  opacity: 0.6;
-  font-weight: lighter;
-}
-.category-item.active{
-  background-color: var(--content-background);
-}
-
 .feature-warp{
   display: flex;
   flex-wrap: wrap;
-  background-color: var(--content-background);
-  padding: 16px 24px;
+  justify-content: center;
+  padding: 24px;
   overflow: hidden;
 }
 .feature-item{
   display: block;
   margin: 10px;
   width: 120px;
-  height: 4rem;
-  line-height: 4rem;
+  height: 90px;
+  line-height: 90px;
   cursor: pointer;
   background: #4f4f4f;
   color: rgb(203, 197, 29);
@@ -115,16 +58,6 @@ const featuresList = computed(() => features.filter((f) => f.category.includes(a
 .feature-item:hover{
   background-color: #555;
   color: rgb(203, 197, 100);
-}
-
-/* 小屏 */
-@media screen and (max-width: 800px) {
-  .index{
-    padding-top: 2rem;
-  }
-  .logo{
-    font-size: 3rem;
-  }
 }
 
 </style>
