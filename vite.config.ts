d5a3b3c2cfb607962273d4b60f1e7665ee8227be
JAY <JAY@oonne.com>
2024-11-26 12:25:42 +0800

    chore: 引入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import { CodeInspectorPlugin } from 'code-inspector-plugin';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    CodeInspectorPlugin({
+      bundler: 'vite',
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
