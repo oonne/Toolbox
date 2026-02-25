5d45e5fdc0147d76da202ed65349f82d34ae7b07
JAY <JAY@oonne.com>
2026-02-25 14:49:09 +0800

    feat: 字数统计

--- src/pages/WordCount/CalcCard.vue
+++ src/pages/WordCount/CalcCard.vue
@@ -1,38 +1,30 @@
 <template>
   <TextInput
-    v-model:text.lazy="input"
-    text-area-class="min-height-6"
+    v-model:text="input"
     placeholder="输入需要统计的文本"
   />
 
   <div class="button-warp">
     <SelectInput
       v-model:selected="chineseMode"
-      label="中文计法"
+      label="中文"
       :options="chineseModeOptions"
     />
-    <ConfirmButton
-      text="统计"
-      :disable="input === ''"
-      @click="onCount"
-    />
-  </div>
-
-  <div class="result">
-    字符数：{{ count }}
+    <div class="result">
+      字符数：{{ count }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import type { SelectOption } from '@/types/type';
 
 const input = ref('');
-const count = ref<number>(0);
 
 const chineseModeOptions: SelectOption[] = [
-  { value: '1', name: '中文=1字符' },
-  { value: '2', name: '中文=2字符' },
+  { value: '1', name: '1字符' },
+  { value: '2', name: '2字符' },
 ];
 const chineseMode = ref('1');
 
@@ -46,15 +38,14 @@ function getCount(text: string, mode: string): number {
   return chineseLen * 2 + (text.length - chineseLen);
 }
 
-const onCount = () => {
-  count.value = getCount(input.value, chineseMode.value);
-};
+const count = computed(() => getCount(input.value, chineseMode.value));
 </script>
 
 <style scoped>
 .button-warp {
   display: flex;
   justify-content: flex-end;
+  align-items: center;
 }
 
 @media screen and (max-width: 680px) {
@@ -67,7 +58,7 @@ const onCount = () => {
 .result {
   margin: 8px 12px;
   padding: 12px;
-  background-color: var(--input-background);
+  font-size: 1.6rem;
   color: var(--color);
 }
 </style>
